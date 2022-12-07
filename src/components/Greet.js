8fa5c213f1ecfad081b1f217b3949c10485be6db
Giovanni Torelli <zapushd@gmail.com>
2022-12-07 20:59:50 -0300

    Criando meu primeiro Stateless Functional Component

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//JavaScript clássico
+//function Greet(){
+//    return <h1>Olá, você!</h1>
+//}
+
+//ES6
+export const Greet = () => <h1>Olá, você!</h1>
+
+//export default Greet
