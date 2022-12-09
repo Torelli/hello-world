0eb6ead1b31eee8a3e69a6cb8d754fef17b8e97f
Giovanni Torelli <zapushd@gmail.com>
2022-12-09 01:18:58 -0300

    Explorando setState e Callback functions

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {Greet} from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App(){
   return(
     <div className="App">
-      <Message/>
-      {/*<Hello name="Giovanni"><p>Você é top!</p></Hello>
+      <Counter/>
+      {/*<Message/>
+      <Hello name="Giovanni"><p>Você é top!</p></Hello>
       <Hello name="José"><p>Você é top!</p></Hello>
       <Hello name="Maria"><p>Você é top!</p></Hello>
       <Greet/>
