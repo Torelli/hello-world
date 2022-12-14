dad18872633c229b9e76c9f96e4029d547353887
Giovanni Torelli <zapushd@gmail.com>
2022-12-14 19:19:04 -0300

    Explorando Conditional Rendering com abordagem Ternary Conditional

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -6,23 +6,29 @@ class UserGreeting extends Component {
     super(props)
   
     this.state = {
-       isLoggedIn: false
+       isLoggedIn: true
     }
   }
   
-  
+  // Abordagem Ternary Condition
     render() {
+      return this.state.isLoggedIn ? (
+        <div>Bem-vindo, Giovanni!</div>
+       ) : (
+        <div>Bem-vindo, Visitante!</div>
+      )
 
-      let message
-      if (this.state.isLoggedIn) {
-        message = <div>Bem-vindo, Giovanni!</div>
-      }
-      else {
-        message = <div>Bem-vindo, Visitante!</div>
-      }
+      // Abordagem com Element Variables
+      // let message
+      // if (this.state.isLoggedIn) {
+      //   message = <div>Bem-vindo, Giovanni!</div>
+      // }
+      // else {
+      //   message = <div>Bem-vindo, Visitante!</div>
+      // }
 
-      return <div>{message}</div>
-
+      // return <div>{message}</div>
+        // Abordagem com If/Else
         // if (this.state.isLoggedIn){
         //     return (
         //         <div>Bem-vindo, Giovanni!</div>
