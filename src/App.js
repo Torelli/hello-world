5b8ee7f9f204aa7b6db70fd3899f5c80d50d3ab0
Giovanni Torelli <zapushd@gmail.com>
2022-12-15 23:55:35 -0300

    Explorando o básico de estilização no react

--- src/App.js
+++ src/App.js
@@ -11,23 +11,33 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
+      {/* Se aplica a todos componentes filhos */}
+      <h1 className='error'>Erro</h1>
+      {/* Escopo local */}
+      <h1 className={styles.success}>Sucesso!</h1>
+      {/*<Inline />
+      <Stylesheet primary={true} />
       <NameList />
-      {/*<UserGreeting/>
-      <ParentComponent/>
-      <EventBind/>
-      <FunctionClick/>
-      <ClassClick/>
+      <UserGreeting />
+      <ParentComponent />
+      <EventBind />
+      <FunctionClick />
+      <ClassClick />
       <Greet name="Bruce" heroName="Batman" />
-      <Message/>
+      <Message />
       <Hello name="Giovanni"><p>Você é top!</p></Hello>
       <Hello name="José"><p>Você é top!</p></Hello>
       <Hello name="Maria"><p>Você é top!</p></Hello>
-      <Greet/>
-      <Welcome name="Fernando"/>*/}
+      <Greet />
+      <Welcome name="Fernando" />*/}
     </div>
   );
 }
